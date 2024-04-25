051376af57d5aa8fef8406b50f592940d31fa02a
ChefanRFG <158303850+ChefanRFG@users.noreply.github.com>
2024-04-24 23:40:41 -0600

    Add files via upload

---
+++ HTfunfiones.py
@@ -0,0 +1,14 @@
+Nombre = ""
+a = 0
+b = 0
+def saludar(Nombre):
+    return Nombre
+Nombre=input("ingrese su nombre: ")
+print(f"Hola, {Nombre} bienvenido a python")
+
+def sumar(a, b):
+    return a + b
+a = input("ingrese su primer numero: ")
+b = input("ingrese su segundo numero: ")
+sum(a, b)
+print(sum)
